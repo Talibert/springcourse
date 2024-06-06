3f5950c4378735217ae949a8e70863e5a30be343
Guilherme Taliberti <guilhermetaliberti@gmail.com>
2024-06-05 22:09:28 -0300

    implementando o método findById

--- src/main/java/com/projetonelio/springcourse/controllers/UserController.java
+++ src/main/java/com/projetonelio/springcourse/controllers/UserController.java
@@ -1,10 +1,15 @@
 package com.projetonelio.springcourse.controllers;
 
 import com.projetonelio.springcourse.entities.User;
+import com.projetonelio.springcourse.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @RestController é uma anotação que combina @Controller com @ResponseBody. Sua função é retornar o objeto e não a view
@@ -14,13 +19,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping (value = "/users")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
     /**
      * @GetMapping irá definir que solicitações do tipo Get que chegarem por essa URL irão cair nesse método
      * @return
      */
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L, "guilherme", "guilhermetaliberti@gmail.com", "telefone", "senha");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> userList = userService.findAll();
+
+        return ResponseEntity.ok().body(userList);
+    }
+
+    /**
+     * @PathVariable diz que o parâmetro que o método recebe virá através da URL. Dessa forma associa o id da URL com o
+     * id do parâmetro
+     * @param id
+     * @return
+     */
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User user = userService.findById(id);
+
+        return ResponseEntity.ok().body(user);
     }
 }
